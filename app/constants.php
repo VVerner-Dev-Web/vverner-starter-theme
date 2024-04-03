a5a79eff8224e39298f24b036abc285e6f934a8e
v-verner <vini.verner1@gmail.com>
2024-04-03 18:01:42 -0300

    vverner - toolbox como dependencia obrigatória

--- app/constants.php
+++
@@ -1,8 +0,0 @@
-<?php defined('ABSPATH') || exit('No direct script access allowed');
-
-define('VV_APP', get_stylesheet_directory() . '/app');
-define('VV_APP_URL', get_stylesheet_directory_uri() . '/app');
-
-// Otimização de imagens - Defina como "0" para não alterar o upload
-define('VV_MAX_IMAGE_SIZE_IN_PIXELS', 1200);
-define('VV_IMAGE_QUALITY', 80);
--- functions.php
+++ functions.php
@@ -1,7 +1,17 @@
-<?php defined('ABSPATH') || exit('No direct script access allowed');
+<?php
 
-require_once __DIR__ . '/app/constants.php';
-require_once __DIR__ . '/app/controller/Core/AutoLoader.php';
+defined('ABSPATH') || exit('No direct script access allowed');
+
+require_once __DIR__ . '/constants.php';
+
+if (!class_exists('VVerner\Core\AutoLoader')) :
+  add_action('admin_notices', function () {
+    echo '<div class="error"><p><strong>Erro:</strong> O site atual depende do plugin VVerner - Toolbox para funcionar. Por favor, contate a equipe VVerner para mais informações.</p></div>';
+  });
+else :
+  (new VVerner\Core\AutoLoader('ThemeName', get_stylesheet_directory() . '/src'))->load();
+endif;
+
 
 /**
  * Insira seus códigos abaixo
